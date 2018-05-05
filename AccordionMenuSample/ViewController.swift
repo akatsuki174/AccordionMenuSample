dcb0ef9561831c134bd33a907a8f15c5d7a12b59
akatsuki174 <dev2015js@gmail.com>
2018-05-05 11:20:10 +0900

    ダミーコンテンツを表示

--- AccordionMenuSample/ViewController.swift
+++ AccordionMenuSample/ViewController.swift
@@ -1,17 +1,25 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "hoge"
+        return cell
+    }
+}
