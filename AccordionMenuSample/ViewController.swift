7a7e8f7318e85176a07b7e3057097ae2fc1673e9
akatsuki174 <dev2015js@gmail.com>
2018-07-14 14:48:47 +0900

    単にrowのプラスとマイナスだけができるサンプル

--- AccordionMenuSample/ViewController.swift
+++ AccordionMenuSample/ViewController.swift
@@ -2,12 +2,10 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    private var viewModel = ViewModel()
+    var rowNum = 4
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        addSectionÇontents()
     }
 
     override func didReceiveMemoryWarning() {
@@ -15,36 +13,36 @@ class ViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func addSectionÇontents() {
-        viewModel.addSectionContent(content: SectionContents(categoryTitle: "1", genreTitles: ["1-1", "1-2", "1-3", "1-4", "1-5"]))
-        viewModel.addSectionContent(content: SectionContents(categoryTitle: "2", genreTitles: ["2-1", "2-2", "2-3", "2-4"]))
-        viewModel.addSectionContent(content: SectionContents(categoryTitle: "3", genreTitles: ["3-1", "3-2", "3-3"]))
-        viewModel.addSectionContent(content: SectionContents(categoryTitle: "4", genreTitles: ["4-1", "4-2", "4-3", "4-4", "4-5"]))
-        viewModel.addSectionContent(content: SectionContents(categoryTitle: "5", genreTitles: ["5-1", "5-2"]))
-        viewModel.addSectionContent(content: SectionContents(categoryTitle: "6", genreTitles: ["6-1", "6-2", "6-3", "6-4", "6-5"]))
+    @IBAction func plusAction(_ sender: Any) {
+        rowNum += 1
+        animate()
+    }
+
+    @IBAction func minusAction(_ sender: Any) {
+        rowNum -= 1
+        animate()
+    }
+
+    private func animate() {
+        tableView.beginUpdates()
+        tableView.reloadSections([0], with: .right)
+        tableView.endUpdates()
     }
 }
 
 extension ViewController {
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return viewModel.sectionCount()
-    }
 
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return viewModel.categoryTitle(section: section)
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.rowCount(section: section)
-    }
-
-    private func cellTitleForRowAtIndexPath(_ indexPath: IndexPath) -> String? {
-        return ""
+        return rowNum
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = cellTitleForRowAtIndexPath(indexPath)
+        cell.textLabel?.text = "cell"
         return cell
     }
 }
